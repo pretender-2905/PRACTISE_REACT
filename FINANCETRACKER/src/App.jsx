034274ffba832f0289ea83ef820ce1ea87c73e3b
pretender-2905 <pretender2905@gmail.com>
2025-05-16 02:01:06 +0500

    commit

--- FINANCETRACKER/src/App.jsx
+++ FINANCETRACKER/src/App.jsx
@@ -4,64 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button'
 import landingPageData from './constants/webData'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
 
-console.log("lampagedata", landingPageData)
+
+
 function App() {
+  console.log("lampagedata", landingPageData)
+  const { header, hero, features, testimonials } = landingPageData
 
   return (
 
     <div>
-      <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-<img src={landingPageData.header.logo} className='h-12 w-12 rounded-full'/>
-      <span className="ml-3 text-xl">{landingPageData.header.title}</span>
-    </a>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">Contact Us</a>
-      <a className="mr-5 hover:text-gray-900">About Us</a>
-   
-    </nav>
-    <Button label ={"Login"} bgColor={"bg-black"} textColor={"text-white"}  borderColor={"border-blue-400"}/>
-  </div>
-</header>
-
-
-<section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-        Before they sold out
-        <br className="hidden lg:inline-block" />
-        readymade gluten
-      </h1>
-      <p className="mb-8 leading-relaxed">
-        Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-        plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk
-        tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard
-        chambray.
-      </p>
-      <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-          Button
-        </button>
-        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-          Button
-        </button>
-      </div>
+      <Header header={header} />
+      <Hero hero={hero} />
+      <Features features={features} />
+      <Testimonials testimonials={testimonials} />
+      <Footer header={header} />
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img
-        className="object-cover object-center rounded"
-        alt="hero"
-        src="https://dummyimage.com/720x600"
-      />
-    </div>
-  </div>
-</section>
 
-    </div>
-    
 
   )
 }
