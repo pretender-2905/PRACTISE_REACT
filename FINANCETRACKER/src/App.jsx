83661b04fc22f6f4bef7761823480f4cd11ca84c
pretender-2905 <pretender2905@gmail.com>
2025-05-17 15:10:48 +0500

    commmit

--- FINANCETRACKER/src/App.jsx
+++ FINANCETRACKER/src/App.jsx
@@ -49,17 +49,51 @@ function App() {
   const handleAddTransaction = () => {
 
     setTransactions([...transactions, {amount, type}])
-    // console.log("Amount: ", amount)
-    // console.log("type: ", type)
+    setAmount("")
+
+    
   }
 console.log("transactions: ", transactions)
+
+ const totalIcome = transactions.reduce(
+  (acc, transaction) => 
+  transaction.type == 'income' ? acc + Number(transaction.amount) : acc,
+  0
+);
+ const totalExpense = transactions.reduce(
+  (acc, transaction) => 
+  transaction.type == 'expense' ? acc + Number(transaction.amount) : acc,
+  0
+);
+
+const balanceAmount = totalIcome - totalExpense
+
+console.log(totalIcome)
+console.log(totalExpense)
+console.log(balanceAmount)
+
+
   return (
     <div>
       <Header header={header} />
 
       <div className='flex flex-col justify-center items-center'>
-        <div className='flex'>
 
+        <div className='flex gap-4 my-4'>
+          <div className='p-2 px-5 rounded-md border border-purple-600'>
+            <h1>Total Income</h1>
+            <h1 className='text-green-600 font-bold'>{totalIcome}</h1>
+          </div>
+          <div className='p-2 px-5 rounded-md border border-purple-600'>
+            <h1>Total Expense</h1>
+            <h1 className='text-red-600 font-bold '>{totalExpense}</h1>
+          </div>
+          <div className='p-2 px-5 rounded-md border border-purple-600'>
+            <h1>Balance</h1>
+            <h1 className= {`font-bold ${balanceAmount < 0 ? 'text-red-600' : 'text-green-600'}`}>{balanceAmount}</h1>
+          </div>
+        </div>
+        <div className='flex'>
           <input
            onChange={(e)=> setAmount(e.target.value)}
             className='border rounded-md border-purple-600 mx-2 p-1'
@@ -87,8 +121,10 @@ console.log("transactions: ", transactions)
          {
           transactions.map((data, index)=>{
             return(
-              <div key={index} className='flex items-center'>
-                <h1 className='font-bold underline text-3xl w-70'>{data.amount}</h1>
+              
+              <div key={index} className='flex items-center my-3'>
+                
+                <h1 className='font-bold underline text-3xl w-70'>{index + 1}{')'}{data.amount}</h1>
                 <h1 className={`font-bold underline text-3xl 
                 ${data.type === "income" ? "text-green-400" : "text-red-400"}`}
                 >{data.type}</h1>
